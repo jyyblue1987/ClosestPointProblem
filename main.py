18c5564378bd24efd3c55675e9008115cb811039
jyyblue <jyyblue1987@outlook.com>
2021-03-19 02:48:01 +0800

    [add] remove unless code.

--- main.py
+++ main.py
@@ -1,116 +1,5 @@
-import math 
-import copy
 
 from random import randrange
-
-# A class to represent a Point in 2D plane 
-class Point(): 
-	def __init__(self, x, y): 
-		self.x = x 
-		self.y = y 
-
-# A utility function to find the 
-# distance between two points 
-def dist(p1, p2): 
-	return math.sqrt((p1.x - p2.x) *
-					(p1.x - p2.x) +
-					(p1.y - p2.y) *
-					(p1.y - p2.y)) 
-
-# A Brute Force method to return the 
-# smallest distance between two points 
-# in P[] of size n 
-# Time complexity: O(n ^ 2)
-def getClosestUsingBruteForce(P): 
-    n = len(P)    
-	
-    min_val = float('inf') 
-    for i in range(n): 
-        for j in range(i + 1, n): 
-            if dist(P[i], P[j]) < min_val: 
-                min_val = dist(P[i], P[j]) 
-
-    return min_val 
-
-# A utility function to find the 
-# distance beween the closest points of 
-# strip of given size. All points in 
-# strip[] are sorted accordint to 
-# y coordinate. They all have an upper 
-# bound on minimum distance as d. 
-# Note that this method seems to be 
-# a O(n^2) method, but it's a O(n) 
-# method as the inner loop runs at most 6 times 
-def stripClosest(strip, size, d): 
-	
-	# Initialize the minimum distance as d 
-	min_val = d 
-
-	
-	# Pick all points one by one and 
-	# try the next points till the difference 
-	# between y coordinates is smaller than d. 
-	# This is a proven fact that this loop 
-	# runs at most 6 times 
-	for i in range(size): 
-		j = i + 1
-		while j < size and (strip[j].y -
-							strip[i].y) < min_val: 
-			min_val = dist(strip[i], strip[j]) 
-			j += 1
-
-	return min_val 
-
-# A recursive function to find the 
-# smallest distance. The array P contains 
-# all points sorted according to x coordinate 
-def closestUtil(P, Q, n): 
-	
-	# If there are 2 or 3 points, 
-	# then use brute force 
-	if n <= 3: 
-		return getClosestUsingBruteForce(P) 
-
-	# Find the middle point 
-	mid = n // 2
-	midPoint = P[mid] 
-
-	# Consider the vertical line passing 
-	# through the middle point calculate 
-	# the smallest distance dl on left 
-	# of middle point and dr on right side 
-	dl = closestUtil(P[:mid], Q, mid) 
-	dr = closestUtil(P[mid:], Q, n - mid) 
-
-	# Find the smaller of two distances 
-	d = min(dl, dr) 
-
-	# Build an array strip[] that contains 
-	# points close (closer than d) 
-	# to the line passing through the middle point 
-	strip = [] 
-	for i in range(n): 
-		if abs(Q[i].x - midPoint.x) < d: 
-			strip.append(Q[i]) 
-
-	# Find the closest points in strip. 
-	# Return the minimum of d and closest 
-	# distance is strip[] 
-	return min(d, stripClosest(strip, len(strip), d)) 
-
-# The main function that finds 
-# the smallest distance. 
-# This method mainly uses closestUtil() 
-def getClosestDivideConquer(P):     
-    P.sort(key = lambda point: point.x) 
-    Q = copy.deepcopy(P) 
-    Q.sort(key = lambda point: point.y)	 
-
-    # Use recursive function closestUtil() 
-    # to find the smallest distance 
-    n = len(P)
-    return closestUtil(P, Q, n) 
-
 
 def euclidean_distance_sqr(point1, point2):
     """
@@ -207,10 +96,6 @@ def closest_pair_of_points_sqr(points_sorted_on_x, points_sorted_on_y, points_co
 
 
 def closest_pair_of_points(points, points_counts):
-    """
-    >>> closest_pair_of_points([(2, 3), (12, 30)], len([(2, 3), (12, 30)]))
-    28.792360097775937
-    """
     points_sorted_on_x = column_based_sort(points, column=0)
     points_sorted_on_y = column_based_sort(points, column=1)
     return (
@@ -230,25 +115,8 @@ def generateRandomPointsAndSaveIt(n):
 
     file.close()
 
+
 def readPointArray():
-    file = open("Points.txt", "r") 
-
-    lines = file.readlines()
-
-    file.close()
-
-    points = []
-    for li in lines:
-       array = li.split(" ")
-       x = int(array[0])
-       y = int(array[1])
-
-       points.append(Point(x, y))
-
-    return points
-
-
-def readPointArray1():
     file = open("Points.txt", "r") 
 
     lines = file.readlines()
@@ -265,16 +133,11 @@ def readPointArray1():
 
     return points
 
-n = 100
-
+# n = 100
 # generateRandomPointsAndSaveIt(n)
 
 points = readPointArray()
-points1 = readPointArray1()
-
 n = len(points) 
 
-print("The smallest distance based on Direct method  is", getClosestUsingBruteForce(points))
-print("The smallest distance based on Direct method1  is", dis_between_closest_pair(points1, len(points1)) ** 0.5)
-print("The smallest distance based on Divide and Conquer  is", getClosestDivideConquer(points))
-print("The smallest distance based on Divide and Conquer1  is", closest_pair_of_points(points1, len(points1)))
+print("The smallest distance based on Direct method  is", dis_between_closest_pair(points, len(points)) ** 0.5)
+print("The smallest distance based on Divide and Conquer1  is", closest_pair_of_points(points, len(points)))

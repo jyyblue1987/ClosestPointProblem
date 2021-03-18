2032c6558e3e8dc34fb2d7fc2a89583f825ae6b1
jyyblue <jyyblue1987@outlook.com>
2021-03-19 03:46:34 +0800

    [add] add refactoring code.

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def dis_using_direct_method(points, n, min_dis=float("inf")):
     return min_dis
 
 # closest pair of points in strip
+# Note that this method seems to be  
+# a O(n^2) method, but it's a O(n)  
+# method as the inner loop runs at most 6 times 
 def strip_closest(points, n, min_dis=float("inf")):
     for i in range(min(6, n - 1), n):
         for j in range(max(0, i - 6), i):
@@ -26,9 +29,18 @@ def strip_closest(points, n, min_dis=float("inf")):
             if current_dis < min_dis:
                 min_dis = current_dis
 
-    return min_dis
+    min_val = min_dis
+
+    # for i in range(n): 
+    #     j = i + 1
+    #     while j < n and (points[j][0]- points[i][0]) < min_val: 
+    #         min_val = dist(points[i], points[j])
+    #         j += 1
 
 
+    return min_val
+
+# T(n)
 def closest_util(s_on_x, s_on_y, n):
     # base case
     if n <= 3:
@@ -36,11 +48,11 @@ def closest_util(s_on_x, s_on_y, n):
 
     # recursion
     mid = n // 2
-    dl = closest_util( s_on_x, s_on_y[:mid], mid)
-    dr = closest_util( s_on_y, s_on_y[mid:], n - mid)
+    dl = closest_util( s_on_x, s_on_y[:mid], mid) # T(n/2)
+    dr = closest_util( s_on_y, s_on_y[mid:], n - mid) # T(n/2)
     d = min(dl, dr)
 
-    strip = []
+    strip = [] # O(n)
     for point in s_on_x:
         if abs(point[0] - s_on_x[mid][0]) < d:
             strip.append(point)
@@ -52,8 +64,8 @@ def closest_util(s_on_x, s_on_y, n):
 
 
 def dis_using_divide_conquer(points, n):
-    s_on_x = sort_based_index(points, column=0)
-    s_on_y = sort_based_index(points, column=1)
+    s_on_x = sort_based_index(points, column=0) # O(N*log(N))
+    s_on_y = sort_based_index(points, column=1) # O(N*log(N))
     return (
         closest_util(
             s_on_x, s_on_y, n
@@ -90,7 +102,7 @@ def readPointArray():
 
     return points
 
-n = 1000
+n = 100
 generateRandomPointsAndSaveIt(n)
 
 points = readPointArray()
@@ -106,3 +118,11 @@ dis = dis_using_divide_conquer(points, len(points))
 end = time.time()
 print("The smallest distance based on Divide and Conquer  is", dis, "Running Time = ", (end - start), 's')
 
+# Time Complexity Let Time complexity of above algorithm be T(n). 
+# Let us assume that we use a O(nLogn) sorting algorithm. 
+# The above algorithm divides all points in two sets and recursively calls for two sets. 
+# After dividing, it finds the strip in O(n) time, sorts the strip in O(nLogn) time and 
+# finally finds the closest points in strip in O(n) time. So T(n) can expressed as follows
+# T(n) = 2T(n/2) + O(n) + O(nLogn) + O(n)
+# T(n) = 2T(n/2) + O(nLogn)
+# T(n) = T(n x Logn x Logn)
